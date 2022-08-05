40b22d4c1a67e95bc6ab8459a5832603737f3095
Samuel Giddins <segiddins@abnormalsecurity.com>
2022-08-04 22:00:11 -0700

    Initial Commit
    Thanks, andre

---
+++ dot_zsh/aliases.zsh
@@ -0,0 +1,13 @@
+alias be="bundle exec"
+alias less=bat
+alias man=batman
+
+alias l="exa --icons"
+alias la="exa -lahF --git --icons"
+alias ll="exa -ll --git --icons"
+alias lt="exa -T"
+
+alias g="git"
+alias gs="git status"
+alias gd="git diff"
+alias gpl="git pull"
---
+++ dot_zsh/fzf.zsh
@@ -0,0 +1,7 @@
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "$BREW_PREFIX/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "$BREW_PREFIX/opt/fzf/shell/key-bindings.zsh"
