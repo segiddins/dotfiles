9eaab7b53b8b79dd1d9303f788bc947f3850c572
Samuel Giddins <segiddins@segiddins.me>
2025-01-27 13:35:47 -0800

    copy stuff from indirect
    Signed-off-by: Samuel Giddins <segiddins@segiddins.me>

---
+++ run_once_before_time_machine_excludes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+set -vx
+
+# don't back up dropbox, steam games
+sudo tmutil addexclusion -p \
+  /Applications/Xcode*.app \
+  "$HOME/Dropbox" \
+  "$HOME/Library/Application Support/Steam/steamapps"
+
+# skip backup on node_modules, bundled gems and rails assets
+pathnames=(~/src/*/*/{.bundle/ruby,node_modules})
+if (find "$pathnames" 2>&1 1>/dev/null); then
+  for pathname in $pathnames; do
+    sudo tmutil addexclusion "$pathname"
+
+    for railspath in tmp/ public/assets public/packs public/packs-test; do
+      railsdir="$pathname/../../$railspath"
+      if [[ -d "$railsdir" ]]; then
+        sudo tmutil addexclusion "$railsdir"
+      fi
+    done
+  done
+fi
